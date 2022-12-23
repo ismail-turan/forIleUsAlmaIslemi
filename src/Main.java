d0af741c15d772cb49cb8adf629dbfacd63435e4
Ismail <natdratuis@gmail.com>
2022-12-23 14:05:03 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        System.out.print("Üslü sayının tabanını giriniz :");
+        double taban=scanner.nextDouble();
+        double result=1;
+        System.out.print("Üslü sayının üssünü giriniz :");
+        double us=scanner.nextDouble();
+        for (int i=1 ; i<=us;i++){
+            result*=taban;
+        }
+        System.out.print("Üslü sonuc :"+(int)result);
+    }
+}
