60577f01c5cea17eac7254abfc21c1c23e7ed599
Ismail <natdratuis@gmail.com>
2022-12-23 14:11:37 +0300

    added Math.pow use

--- src/Main.java
+++ src/Main.java
@@ -2,15 +2,22 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner=new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
         System.out.print("Üslü sayının tabanını giriniz :");
-        double taban=scanner.nextDouble();
-        double result=1;
+        double taban = scanner.nextDouble();
+        double result = 1;
         System.out.print("Üslü sayının üssünü giriniz :");
-        double us=scanner.nextDouble();
-        for (int i=1 ; i<=us;i++){
-            result*=taban;
+        double us = scanner.nextDouble();
+        for (int i = 1; i <= us; i++) {
+            result *= taban;
         }
-        System.out.print("Üslü sonuc :"+(int)result);
+        System.out.print("Üslü sonuc :" + (int) result);
+        System.out.println();
+
+        //şöyle de olabilirdi=============================
+        double result2=Math.pow(taban,us);
+        System.out.print("Mat.pow ile hesaplanan değer :"+(int)result2);
+
+
     }
 }
